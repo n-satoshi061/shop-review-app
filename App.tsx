13eee1446083ac66638c1531f8cb939e5377b127
n-satoshi061 <n.satoshi061@gmail.com>
2020-10-16 16:48:09 +0900

    ユーザー認証の実装

--- App.tsx
+++ App.tsx
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from "react";
 import {AppNavigator} from './src/navigation/AppNavigator'
-
+import {UserContext} from './src/contexts/userContext';
+import {User} from './src/types/user'
 
 export default function App() {
-  return <AppNavigator />
+  const [user, setUser] = useState<User>();
+
+  return (
+    <UserContext.Provider value={{user, setUser}}>
+      <AppNavigator />
+    </UserContext.Provider>
+  )
 }
 
