2831a4800a1f9aca02b211fe8f0fe3f66764f19c
n-satoshi061 <n.satoshi061@gmail.com>
2020-10-18 17:13:18 +0900

    firestoreのデータ更新

--- App.tsx
+++ App.tsx
@@ -1,15 +1,22 @@
-import React, { useEffect, useState } from "react";
-import {AppNavigator} from './src/navigation/AppNavigator'
-import {UserContext} from './src/contexts/userContext';
-import {User} from './src/types/user'
+import React, { useState } from "react";
+import { AppNavigator } from "./src/navigation/AppNavigator";
+/* contexts */
+import { UserContext } from "./src/contexts/userContext";
+import { ReviewsContext } from "./src/contexts/reviewsContext";
+/* types  */
+import { User } from "./src/types/user";
+import { Review } from "./src/types/review";
 
 export default function App() {
   const [user, setUser] = useState<User>();
+  const [reviews, setReviews] = useState<Review[]>([]);
 
   return (
-    <UserContext.Provider value={{user, setUser}}>
-      <AppNavigator />
+    <UserContext.Provider value={{ user, setUser }}>
+      <ReviewsContext.Provider value={{ reviews, setReviews }}>
+        <AppNavigator />
+      </ReviewsContext.Provider>
     </UserContext.Provider>
-  )
+  );
 }
 
